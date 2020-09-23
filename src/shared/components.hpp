7455a0f7723c6cb2ad8c5932699d33b11d19667d
Isabel <isabelgk@users.noreply.github.com>
2020-09-23 12:36:28 -0500

    Add attenuverters to Werner filter CV inputs (#5)

--- src/shared/components.hpp
+++ src/shared/components.hpp
@@ -13,6 +13,15 @@ struct ChowKnob : app::SvgKnob {
     }
 };
 
+struct ChowSmallKnob : app::SvgKnob {
+    ChowSmallKnob() {
+        minAngle = -0.76 * M_PI;
+        maxAngle = 0.76 * M_PI;
+        shadow->opacity = 0;
+        setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/SmallKnob.svg")));
+    }
+};
+
 struct ChowPort : app::SvgPort {
     ChowPort() {
         setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/Port.svg")));
