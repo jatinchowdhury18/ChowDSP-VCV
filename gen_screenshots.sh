765af8f0faada6e8b880976faf7559e81f165df3
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-11-03 00:38:34 +0000

    Update screenshots

--- gen_screenshots.sh
+++ gen_screenshots.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    RACK_USER_DIR="$HOME/Documents/Rack2"
+    RACK_EXE="/Applications/VCV Rack 2.app/Contents/MacOS/Rack"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    RACK_USER_DIR="/c/Users/jatin/OneDrive/Documents/Rack2"
+    RACK_EXE="/c/Program\ Files/VCV/Rack2/Rack.exe"
+else
+    echo "Script not implemented for this OS!"
+    exit
+fi
+
 # delete existing screenshots
 rm doc/Screenshots/*.png
-rm /c/Users/jatin/OneDrive/Documents/Rack/screenshots/ChowDSP/*
+rm $RACK_USER_DIR/screenshots/ChowDSP/*
 
 # generate screenshots
-/c/Program\ Files/VCV/Rack/Rack.exe -t 2
+"$RACK_EXE" -t 2
 
 # copy new screenshots
-cp /c/Users/jatin/OneDrive/Documents/Rack/screenshots/ChowDSP/*.png doc/Screenshots/
+cp $RACK_USER_DIR/screenshots/ChowDSP/*.png doc/Screenshots/
